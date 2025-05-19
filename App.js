f64a24918a227fde0dc97b4eeb7589e8a578de25
Szigetvári Barnabás <barnabszigetvari@gmail.com>
2025-05-19 22:12:19 +0200

    update

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Gyar Management System</h1>
+      <p>This is the frontend interface running on Render.</p>
+    </div>
+  );
+}
+
+export default App;
